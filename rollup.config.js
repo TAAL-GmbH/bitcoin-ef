c5d14251c094e53ca3b42ebcdcb74dd3c464e67f
Simon Ordish <simon.ordish@masagi.co.uk>
2024-08-19 13:58:34 +0200

    fix extension

--- rollup.config.js
+++ rollup.config.js
@@ -77,7 +77,7 @@ export default [
     input: "src/cli.ts",
     output: [
       {
-        file: 'dist/cli.js',
+        file: 'dist/cli.cjs',
         format: "cjs",
         sourcemap: true,
         banner: '#!/usr/bin/env node',
